164fcdbf53d95660f52381e774cd839a5be756d2
Vladimir Tolstopyatov <tolstopyatovvs@gmail.com>
2023-11-30 22:00:04 +0300

    fix list from getenv

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,9 +9,9 @@ SECRET_KEY = os.getenv('DJANGO_KEY')
 
 DEBUG = strtobool(os.getenv('DEBUG', 'False'))
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS'),]
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS'),]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
